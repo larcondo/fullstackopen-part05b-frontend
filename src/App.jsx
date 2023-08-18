299468e64c96c94287340370e434d8591b6fd29f
larcondo <larcondo90@gmail.com>
2023-08-18 18:23:41 -0300

    first modification part2.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-function App() {
+import Note from "./components/Note"
 
+function App({ notes }) {
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        { notes.map( note => <Note key={note.id} note={note} />)}
+      </ul>
+    </div>
   )
 }
 

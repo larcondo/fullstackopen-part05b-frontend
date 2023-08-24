dfd3f42fe1d500affb70f401f3d124df21de038f
larcondo <larcondo90@gmail.com>
2023-08-24 16:44:07 -0300

    hide noteForm after create new note (useRef hook)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 // import axios from 'axios'
 import noteService from './services/notes'
 import loginService from './services/login'
@@ -17,6 +17,7 @@ function App() {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [loginVisible, setLoginVisible] = useState(false)
+  const noteFormRef = useRef()
 
   const hook = () => {
     noteService.getAll()
@@ -38,6 +39,7 @@ function App() {
   }, [])
 
   const addNote = (noteObject) => {
+    noteFormRef.current.toggleVisibility()
     noteService.create(noteObject)
     .then(returnedNote => {
       setNotes(notes.concat(returnedNote))
@@ -114,7 +116,7 @@ function App() {
   }
 
   const noteForm = () => (
-    <Togglable buttonLabel="new note">
+    <Togglable buttonLabel="new note" ref={noteFormRef} >
       <NoteForm createNote={addNote} />
     </Togglable>
   )
